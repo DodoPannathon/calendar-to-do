e1a1d68cd2e8b33593b68756a6382b34589ee9d9
DodoPannathon <okur34910@gmail.com>
2025-05-06 15:08:23 +0700

    add task but it Unavailable now

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const today = new Date(); // วันที่ปัจจุบัน
-    const currentDay = today.getDate(); // ดึงวันที่ (1-31)
+    const today = new Date();
+    const currentDay = today.getDate();
 
-    // ค้นหา <div> ที่มีตัวเลขตรงกับวันที่ปัจจุบัน
     const numbers = document.querySelectorAll(".number");
     numbers.forEach((number) => {
         if (parseInt(number.textContent) === currentDay) {
-            number.parentElement.classList.add("current-day"); // เพิ่มคลาสให้ <td>
+            number.parentElement.classList.add("current-day");
         }
     });
-});
+});function displayaddtask() {
+    const form = document.createElement("form");
+    const addtask = document.querySelector('.add-task')
+    form.setAttribute("id", "addtaskform");
+    form.innerHTML = `
+    <input type="text" id="task-input" placeholder="Enter your task here..." required>
+    <button type="submit">Add Task</button>
+    `
+    form.style.position = 'fixed';
+    form.style.backgroundColor = 'black'
+    form.style.padding = '10px';
+    form.style.borderRadius = '20px';
+    form.style.zIndex = '10';
+    form.style.border = '1px solid white'
+    form.style.marginTop = '5px'
+    addtask.appendChild(form);
+}

0b0839228ac74ebe6a07dfd7e80d2712f8e4a521
DodoPannathon <okur34910@gmail.com>
2025-05-11 22:25:17 +0700

    final display but unavailable

--- script.js
+++ script.js
@@ -8,20 +8,63 @@ document.addEventListener("DOMContentLoaded", () => {
             number.parentElement.classList.add("current-day");
         }
     });
-});function displayaddtask() {
+});
+function displayaddtask() {
     const form = document.createElement("form");
     const addtask = document.querySelector('.add-task')
     form.setAttribute("id", "addtaskform");
     form.innerHTML = `
-    <input type="text" id="task-input" placeholder="Enter your task here..." required>
-    <button type="submit">Add Task</button>
+    <div class="close">
+        <button id="close" type="button">X</button>
+    </div>
+    <div class="input-field">
+        <input type="text" id="task-input"required>
+        <label for="task-input">Enter you Task</label>
+    </div>
+    <button id="repeat-task" type="button" onclick="displayrepeat()">Repeat</button>
+    <button id="add-task" type="submit">Add Task</button>
     `
     form.style.position = 'fixed';
-    form.style.backgroundColor = 'black'
+    form.style.backgroundColor = '#222222'
     form.style.padding = '10px';
     form.style.borderRadius = '20px';
     form.style.zIndex = '10';
-    form.style.border = '1px solid white'
     form.style.marginTop = '5px'
     addtask.appendChild(form);
+
+    const close = form.querySelector("#close")
+    close.addEventListener('click', () => {
+        form.remove();
+    });
+};
+function displayrepeat() {
+    const repeat = document.createElement("div");
+    const form = document.querySelector('#addtaskform');
+    repeat.setAttribute("id", "repeat");
+    repeat.innerHTML = `
+    <div class="repeat-container">
+        <button type="button" class=" duration-repeat-button" id="repeat-day">Repeat every 1 day</button>
+    </div>
+    <div class="repeat-container">
+        <button type="button" class=" duration-repeat-button" id="repeat-week">Repeat every 1 week</button>
+    </div>
+    <div class="repeat-container">
+        <button type="button" class=" duration-repeat-button" id="repeat-month">Repeat every 1 month</button>
+    </div>
+    <div class="repeat-container">
+        <button type="button" class=" duration-repeat-button" id="repeat-custom">Custom Repeat</button>
+    </div>
+    `;
+    repeat.style.position = 'fixed';
+    repeat.style.backgroundColor = '#313131';
+    repeat.style.padding = '10px';
+    repeat.style.borderRadius = '20px';
+    repeat.style.zIndex = '10';
+    repeat.style.marginTop = '5px';
+    form.appendChild(repeat);
+
+    repeat.focus();
+    repeat.addEventListener('blur', () => {
+        repeat.remove();
+    });
 }
